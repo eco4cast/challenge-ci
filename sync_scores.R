c637b9b30ca4dc1d07b1dd20f459ce7db22118ca
rqthomas <rqthomas@vt.edu>
2022-03-21 12:56:17 +0000

    updates from the server

--- sync_scores.R
+++ sync_scores.R
@@ -1,1 +1,4 @@
-aws.s3::s3sync("/efi_neon_challenge/local/scores",bucket = "scores", direction = "download")
+
+Sys.setenv("AWS_DEFAULT_REGION"="data")
+Sys.setenv("AWS_S3_ENDPOINT"="ecoforecast.org")
+aws.s3::s3sync("/efi_neon_challenge/local/cb",bucket = "scores", direction = "download")
