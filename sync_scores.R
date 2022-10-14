a5e8c1c0dc7265c7198c286b7b5d9ed2fb8fdebf
rqthomas <rqthomas@vt.edu>
2022-10-14 02:01:43 +0000

    sync

--- sync_scores.R
+++ sync_scores.R
@@ -11,14 +11,22 @@ challenge_config <- yaml::read_yaml("challenge_config.yml")
 
 themes <- names(challenge_config$themes)
 
+days90 <- as.character(Sys.Date() - lubridate::days(90))
+
+unlink(file.path(challenge_config$DATA_DIR, "scores/parquet/"), recursive = TRUE)
 
 library(arrow)
 for(i in 1:length(themes)){
+  message(themes[i])
 s3 <- s3_bucket(paste0("neon4cast-scores/parquet/", themes[i]), endpoint_override=challenge_config$endpoint_override)
 all_scores <- open_dataset(s3)
+if(themes[i] %in% c("aquatics","phenology","terrestrial_30min","terrestril_daily")){
+  all_scores <- all_scores |> 
+    dplyr::filter(reference_datetime > days90)
+}
 write_dataset(all_scores,
               file.path(challenge_config$DATA_DIR, "scores/parquet/", themes[i]),
-              format = 'parquet',  partitioning=c("model_id", "reference_datetime", "site_id"))
+              partitioning=c("model_id", "reference_datetime", "site_id"))
 }
 
 gc()
