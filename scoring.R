a8f07178d62a0c115338b7470df35cd5dfd10882
Carl <cboettig@gmail.com>
2022-11-23 17:35:53 +0000

    new scoring

--- scoring.R
+++ scoring.R
@@ -15,21 +15,21 @@ s3_scores <- arrow::s3_bucket("neon4cast-scores", endpoint_override = endpoint)
 s3_prov <- arrow::s3_bucket("neon4cast-prov", endpoint_override = endpoint)
 
 
-time <- score_theme("beetles", s3_forecasts, s3_targets, s3_scores, s3_prov, max_horizon = 365L)
+time <- score_theme("beetles", s3_forecasts, s3_targets, s3_scores, s3_prov)
 message(paste("beetles done in", time[["real"]]))
 
-time <- score_theme("ticks", s3_forecasts, s3_targets, s3_scores, s3_prov, max_horizon = 365L)
+time <- score_theme("ticks", s3_forecasts, s3_targets, s3_scores, s3_prov)
 message(paste("ticks done in", time[["real"]]))
 
-time <- score_theme("aquatics", s3_forecasts, s3_targets, s3_scores, s3_prov, max_horizon = 35L)
+time <- score_theme("aquatics", s3_forecasts, s3_targets, s3_scores, s3_prov)
 message(paste("aquatics done in", time[["real"]]))
 
-time <- score_theme("terrestrial_daily", s3_forecasts, s3_targets, s3_scores, s3_prov, max_horizon = 35L)
+time <- score_theme("terrestrial_daily", s3_forecasts, s3_targets, s3_scores, s3_prov)
 message(paste("terrestrial_daily done in", time[["real"]]))
 
-time <- score_theme("phenology", s3_forecasts, s3_targets, s3_scores, s3_prov, max_horizon = 35L)
+time <- score_theme("phenology", s3_forecasts, s3_targets, s3_scores, s3_prov)
 message(paste("phenology done in", time[["real"]]))
 
-#time <- score_theme("terrestrial_30min", s3_forecasts, s3_targets, s3_scores, s3_prov, max_horizon = 35L)
-#message(paste("terrestrial_30min done in", time[["real"]]))
+time <- score_theme("terrestrial_30min", s3_forecasts, s3_targets, s3_scores, s3_prov)
+message(paste("terrestrial_30min done in", time[["real"]]))
 
