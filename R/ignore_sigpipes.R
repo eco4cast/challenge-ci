2048c0560c813f9f2629cd51a087259d2f323908
Carl <cboettig@gmail.com>
2023-07-09 23:28:35 +0000

    s3 inventory

--- R/ignore_sigpipes.R
+++ R/ignore_sigpipes.R
@@ -1,10 +1,15 @@
-library(decor)
-library(cpp11)
-cpp11::cpp_source(code = '
-#include <csignal>
-#include <cpp11.hpp>
-[[cpp11::register]] void ignore_sigpipes() {
-  signal(SIGPIPE, SIG_IGN);
+#' ignore sigpipe
+#' 
+#' Avoid SIGPIPE error in scripts using Arrow S3
+#' @export
+ignore_sigpipe <- function() {
+  requireNamespace("decor", quietly = TRUE)
+  cpp11::cpp_source(code = '
+  #include <csignal>
+  #include <cpp11.hpp>
+  [[cpp11::register]] void ignore_sigpipes() {
+    signal(SIGPIPE, SIG_IGN);
+  }
+  ')
+  ignore_sigpipes()
 }
-')
-ignore_sigpipes()
